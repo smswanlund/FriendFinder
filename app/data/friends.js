c9f6b14bd1e618d67df41379b4ca9952bffb5fe4
smswanlund <muelmac@gmail.com>
2019-11-05 23:30:30 -0500

    html, paths, logic

---
+++ app/data/friends.js
@@ -0,0 +1,10 @@
+//data storage
+var friendArray = [
+    {
+        name: "Thanos",
+        photo: "https://upload.wikimedia.org/wikipedia/en/c/cd/Thanos_Infinity_4.png",
+        scores: ["","","","","","","","","",""]
+    }
+];
+
+module.exports = friendArray;
